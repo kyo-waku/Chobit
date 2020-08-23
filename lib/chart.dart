a2dd9f28dab2ea168017810b7a516a0f531368bd
sugitlab <sgmt.snj@gmail.com>
2020-08-23 22:48:08 +0900

    3ページの切り替え対応

---
+++ lib/chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (Center(
+      child: Container(
+        color: Colors.blue,
+        width: 500,
+        height: 500,
+        child: Text('Chart Widget'),
+      ),
+    ));
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (Center(
+      child: Container(
+        color: Colors.blue,
+        width: 500,
+        height: 500,
+        child: Text('Settings Widget'),
+      ),
+    ));
+  }
+}
