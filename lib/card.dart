73c0578a5d69d2d54eb8e2e2ce8f32f0db4cf9ae
sugitlab <sgmt.snj@gmail.com>
2020-09-11 00:13:23 +0900

    flipcard + animations

--- lib/card.dart
+++ lib/card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
 class CardWidget extends StatefulWidget {
   CardWidget({Key key}) : super(key: key);
@@ -8,12 +9,15 @@ class CardWidget extends StatefulWidget {
 
 class _CardWidgetState extends State<CardWidget> {
   int _cardViewMode = 0; // 0 : Swipable, 1: Arranged
+  Widget _currentCardView = SwipableCardWidget();
   void _onCardViewModeTapped() {
     setState(() {
       if (_cardViewMode == 0) {
         _cardViewMode = 1;
+        _currentCardView = ArrangedCardWidget();
       } else {
         _cardViewMode = 0;
+        _currentCardView = SwipableCardWidget();
       }
     });
   }
@@ -42,7 +46,10 @@ class _CardWidgetState extends State<CardWidget> {
                   .apply(fontSizeFactor: 1.5, fontWeightDelta: 2),
             ),
           ),
-          _cardViewMode == 0 ? SwipableCardWidget() : ArrangedCardWidget()
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 400),
+            child: _currentCardView,
+          ),
         ],
       ),
     ));
@@ -57,70 +64,156 @@ class SwipableCardWidget extends StatefulWidget {
 
 class _SwipableCardState extends State<SwipableCardWidget> {
   int _index = 0;
-  bool _trash = false;
+
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
   @override
   Widget build(BuildContext context) {
     return (GestureDetector(
-      onPanUpdate: (DragUpdateDetails dud) {
-        if (dud.delta.dy > 30) {
-          setState(() {
-            _trash = true;
-          });
-        }
-        if (dud.delta.dy < -30) {
-          setState(() {
-            _trash = false;
-          });
-        }
-      },
+      onTap: () {},
       child: Center(
-        child: Column(
-          children: <Widget>[
-            _trash
-                ? IconButton(
-                    icon: Icon(
-                      Icons.delete_forever,
-                      size: 36.0,
-                    ),
-                    tooltip: 'Delete',
-                    onPressed: () {
-                      setState(() {
-                        _trash = false;
-                      });
-                    },
-                  )
-                : SizedBox(height: 0),
-            SizedBox(
-              height: 600, // card height
-              child: PageView.builder(
-                itemCount: 10,
-                controller: PageController(viewportFraction: 0.8),
-                onPageChanged: (int index) => setState(() => {
-                      _index = index,
-                      _trash = false,
-                    }),
-                itemBuilder: (_, i) {
-                  return (Transform.scale(
-                    scale: i == _index ? 0.88 : 0.9,
-                    child: CardWithIconBackground(),
-                  ));
-                },
-              ),
-            ),
-          ],
+        child: SizedBox(
+          height: 600, // card height
+          child: PageView.builder(
+            itemCount: 10,
+            controller: PageController(viewportFraction: 0.8),
+            onPageChanged: (int index) => setState(() => {
+                  _index = index,
+                }),
+            itemBuilder: (_, i) {
+              return (Transform.scale(
+                scale: i == _index ? 0.9 : 0.9,
+                child: FlipCard(
+                  direction: FlipDirection.HORIZONTAL,
+                  front: CardHistoryMode(),
+                  back: CardRecordMode(),
+                ),
+              ));
+            },
+          ),
         ),
       ),
     ));
   }
 }
 
-class CardWithIconBackground extends StatefulWidget {
-  CardWithIconBackground({Key key}) : super(key: key);
+class CardRecordMode extends StatefulWidget {
+  CardRecordMode({Key key}) : super(key: key);
   @override
-  _CardWithIconBackgroundState createState() => _CardWithIconBackgroundState();
+  _CardRecordModeState createState() => _CardRecordModeState();
 }
 
-class _CardWithIconBackgroundState extends State<CardWithIconBackground> {
+class _CardRecordModeState extends State<CardRecordMode> {
+  @override
+  Widget build(BuildContext context) {
+    return (Card(
+      color: Colors.green[200],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Opacity(
+              opacity: 0.5,
+              child: Icon(
+                Icons.directions_run,
+                color: Colors.blueGrey,
+                size: 200.0,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Spacer(flex: 2),
+                  Text(
+                    'Running',
+                    style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(flex: 2),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.brightness_low,
+                            size: 50,
+                            color: Colors.blueGrey,
+                          ),
+                          Text(
+                            'Excellent',
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.thumb_up,
+                            size: 50,
+                            color: Colors.blueGrey,
+                          ),
+                          Text(
+                            'Nice',
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(flex: 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.copyright,
+                            size: 50,
+                            color: Colors.blueGrey,
+                          ),
+                          Text(
+                            'Chobit',
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.local_cafe,
+                            size: 50,
+                            color: Colors.blueAccent,
+                          ),
+                          Text(
+                            'Break',
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(flex: 2),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class CardHistoryMode extends StatefulWidget {
+  CardHistoryMode({Key key}) : super(key: key);
+  @override
+  _CardHistoryModeState createState() => _CardHistoryModeState();
+}
+
+class _CardHistoryModeState extends State<CardHistoryMode> {
   @override
   Widget build(BuildContext context) {
     return (Card(
@@ -176,18 +269,18 @@ class ArrangedCardWidget extends StatelessWidget {
           spacing: 10,
           runSpacing: 10,
           children: <Widget>[
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
-            SquareCardWithIconBackground(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
+            SquareCard(),
           ],
         ),
       ),
@@ -195,15 +288,13 @@ class ArrangedCardWidget extends StatelessWidget {
   }
 }
 
-class SquareCardWithIconBackground extends StatefulWidget {
-  SquareCardWithIconBackground({Key key}) : super(key: key);
+class SquareCard extends StatefulWidget {
+  SquareCard({Key key}) : super(key: key);
   @override
-  _SquareCardWithIconBackgroundState createState() =>
-      _SquareCardWithIconBackgroundState();
+  _SquareCard createState() => _SquareCard();
 }
 
-class _SquareCardWithIconBackgroundState
-    extends State<SquareCardWithIconBackground> {
+class _SquareCard extends State<SquareCard> {
   @override
   Widget build(BuildContext context) {
     double cardScale = 180;
